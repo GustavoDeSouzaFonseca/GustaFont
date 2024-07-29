ee82f848d91e745d4d99fac468a205aa4e7ec6d0
GustavoDeSouzaFonseca <gustavode.s.fonseca@gmail.com>
2024-07-29 18:25:10 -0300

    refactor: :iphone: Adjust resize from text at header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ toggleLanguage, language, text}) {
 
   return (
     <>
-      <header className='bg-transparent w-full h-16 px-10 py-5 grid grid-cols-2 bg-gradient-to-br from-slate-950 to-[#1e111d] fixed z-20'>
+      <header className='bg-transparent w-full h-16 md:px-10 px-5 py-5 grid grid-cols-2 bg-gradient-to-br from-slate-950 to-[#1e111d] fixed z-20'>
         <a href='#main' className='font-bold gradient-text text-xl font-mono'>{'GustaFont </>'}</a>
 
         <div className='flex flex-row justify-around text-white'>
@@ -14,7 +14,8 @@ export default function Header({ toggleLanguage, language, text}) {
             className='
               hover:font-semibold hover:gradient-text hover:cursor-pointer 
               xl:text-base xl:p-0 
-              text-sm p-1
+              md:text-sm 
+              text-[10px] mt-1 p-1
             '
           >
             {about}
@@ -24,7 +25,8 @@ export default function Header({ toggleLanguage, language, text}) {
             className='
               hover:font-semibold hover:gradient-text hover:cursor-pointer 
               xl:text-base xl:p-0 
-              text-sm p-1
+              md:text-sm  
+              text-[10px] mt-1 p-1
             '
           >
             {projects}
@@ -33,8 +35,9 @@ export default function Header({ toggleLanguage, language, text}) {
             href='#contacs'
             className='
               hover:font-semibold hover:gradient-text hover:cursor-pointer 
-              xl:text-base xl:p-0 
-              text-sm p-1
+              xl:text-base xl:p-0
+              md:text-sm 
+              text-[10px] mt-1 p-1
             '
           >
             {contacts}
@@ -44,7 +47,8 @@ export default function Header({ toggleLanguage, language, text}) {
             className='
               hover:font-semibold  hover:cursor-pointer
               bg-gradient-to-tr from-yellow-500 via-pink-600 to-purple-950 
-              w-20 h-8 rounded-xl 
+              md:w-20 md:text-base
+              w-12 text-[12px] h-8 rounded-xl 
             '
           >
             {language}
